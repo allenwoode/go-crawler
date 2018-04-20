5e4abeea4ec5995dc32e229fd4184367a684f4c0
eeyoo <feilinwu2036@gmail.com>
2018-04-20 11:23:08 +0800

    regex

--- golang/basic/regex/regex.go
+++ golang/basic/regex/regex.go
@@ -8,8 +8,8 @@ import (
 const text = `
 my email is gfw.crack.out@gmail.com@qq.com
 abc@golang.org
-    47591130x@qq.com
-		ddd.fe@google.co.jp
+    47x@qq.com
+ddd.fe56@123google.co.jp
 `
 func main() {
 	re := regexp.MustCompile(`([.a-zA-Z0-9]+)@([a-zA-Z0-9]+)(\.[a-zA-Z0-9.]+)`)
